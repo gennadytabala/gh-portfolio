4d445dff44d8cc2c5fe4beed9a28b6322a65ac02
gennadytabala <45870843+gennadytabala@users.noreply.github.com>
2022-11-26 22:09:23 +0200

    head title, meta added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { environment } from '../environments/environment';
+import {Title, Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,24 @@ import { environment } from '../environments/environment';
 
 export class AppComponent {
   username = environment.username;
+  
+  constructor(private title: Title, private meta:Meta){}
+
+  ngOnInit() {
+    this.title.setTitle('GitHub portofolio app');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content:`${this.username}'s GitHub portofolio`
+      },
+      {
+        name: 'author',
+        content:this.username
+      }
+
+
+    ]);
+  }
+
 }
+
